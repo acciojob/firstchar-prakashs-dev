7fe665ddf47273f72f000a7425935f74bcf13600
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-14 11:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
   // your code here
+	if(text == " " || text == "") return "";
+    text = text.trim();
+    return text.charAt(0);
 }
-
 // Do not change the code below
-
 const text = prompt("Enter text:");
 alert(firstChar(text));
